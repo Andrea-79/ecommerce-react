316d429462b8f6be6019630b67d2cd60f8b61212
Andrea <andreagorozo@hotmail.com>
2024-06-18 19:42:32 -0300

    re-entrega

--- code-vite-app/src/App.jsx
+++ code-vite-app/src/App.jsx
@@ -1,28 +1,23 @@
-import './App.css'
-import { useState } from 'react'
-import Navbar from './components/navbar/navbar'
-import ItemListContainer from './containers/itemListContainer/itemListContainer'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import ItemListContainer from './containers/itemListContainer/itemListContainer';
+import ItemDetailContainer from './containers/itemDetailContainer/itemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
-  const [categoria, setCategoria] = useState('todos')
-            
-  return(
-    
-    <>
-    <BrowserRouter>
-    < Navbar handleCategoria={setCategoria}/>
-
-      <Routes>
-        <Route path='/' element={<ItemListContainer />} />
-        <Route path='/category/:idCategory' element={<ItemListContainer />} />
-        <Route path='/contacto' element={<contacto />} />
-
-        </Routes>
-    </BrowserRouter>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<ItemListContainer />} />
+                    <Route path='/category/:idCategory' element={<ItemListContainer />} />
+                    <Route path='/product/:idProduct' element={<ItemDetailContainer />} />
+                    <Route path='/contacto' element={<Contacto />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
--- code-vite-app/src/containers/itemListContainer/itemListContainer.jsx
+++ code-vite-app/src/containers/itemListContainer/itemListContainer.jsx
@@ -1,31 +1,38 @@
-import {useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { useParams, Link, Routes, Route } from 'react-router-dom';
+import ItemDetailContainer from './itemDetailContainer.jsx';
 
-const ItemListContainer = ({categoria}) => {
-
-    const [products, setProducts] = useState([])
-    const {idCategory} = useParams() 
+const ItemListContainer = () => {
+    const [products, setProducts] = useState([]);
+    const { idCategory } = useParams();
 
     useEffect(() => {
+        const url = idCategory 
+            ? `https://fakestoreapi.com/products/category/${idCategory}`
+            : 'https://fakestoreapi.com/products';
 
-        if(categoria === 'todos'){
-        fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>setProducts(json))
-            .catch(e => console.error (e))
-        } else {
-            fetch(`https://fakestoreapi.com/products/category/${categoria}`)
-            .then(res=>res.json())
-            .then(json=>setProducts(json))
-            .catch(e => console.error (e))
-        }
-    }, [categoria])
-    
+        fetch(url)
+            .then(res => res.json())
+            .then(json => setProducts(json))
+            .catch(e => console.error(e));
+    }, [idCategory]);
+
     return (
         <div>
-            {products.map((product) => <p key={product.id} > {product.title}</p>)}
+            <Routes>
+                <Route path=":idProduct" element={<ItemDetailContainer />} />
+                <Route path="/" element={
+                    <>
+                        {products.map((product) => (
+                            <p key={product.id}>
+                                <Link to={`/product/${product.id}`}>{product.title}</Link>
+                            </p>
+                        ))}
+                    </>
+                } />
+            </Routes>
         </div>
-    )
-
+    );
 }
-export default ItemListContainer
+
+export default ItemListContainer;
